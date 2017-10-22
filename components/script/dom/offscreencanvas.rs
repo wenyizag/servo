6024a25e942d320f5684fb6b28dd699a217977ca
Wenyi Zhang <wzhang43@ncsu.edu>
2017-10-21 21:53:33 -0400

    create offscreencanvas.rs

---
+++ components/script/dom/offscreencanvas.rs
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use dom::bindings::codegen::Bindings::OffscreenCanvasBinding;
+use dom::bindings::codegen::Bindings::OffscreenCanvasBinding::OffscreenCanvasMethods;
+use dom::bindings::reflector::{Reflector, reflect_dom_object};
+use dom::eventtarget::EventTarget;
+use dom::window::Window;
+use dom_struct::dom_struct;
+
+
+#[dom_struct]
+pub struct OffscreenCanvas {
+}
+
+impl OffscreenCanvas {
+}
+
+impl OffscreenCanvasMethods for OffscreenCanvas {
+} 
